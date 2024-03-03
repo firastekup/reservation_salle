db09abc16ef3311a43aa2417c034a133b2e663fd
firas.houaidi@tek-up.de firas houaidi <houaidifiras.tekup@gmail.com>
2024-03-03 01:51:01 +0100

    date fixed

--- controllers/reservation.controller.js
+++ controllers/reservation.controller.js
@@ -7,24 +7,48 @@ exports.getReservationPage = (req, res, next) => {
     res.render('reservation');
 };
 
-exports.postReservationData = (req, res, next) => {
-    // Enregistrer la réservation dans la base de données
-    const { startDate, endDate, roomId } = req.body;
-    const userId = req.session.userId;
+exports.postReservationData = async (req, res, next) => {
+    try {
+        const { startDate, endDate, roomId } = req.body;
+        const userId = req.session.userId;
 
-    const reservation = new Reservation({
-        startDate: startDate,
-        endDate: endDate,
-        roomId: roomId,
-        userId: userId
-    });
+        // Vérifier si la salle est disponible pour les dates spécifiées
+        const isRoomAvailable = await checkRoomAvailability(startDate, endDate, roomId);
 
-    reservation.save()
-        .then(() => {
-            res.redirect('/dashboard'); // Rediriger vers le tableau de bord ou une autre page
-        })
-        .catch((error) => {
-            console.log(error);
-            // Gérer les erreurs de manière appropriée
+        if (!isRoomAvailable) {
+            return res.status(400).send("La salle sélectionnée n'est pas disponible pour ces dates.");
+        }
+
+        const reservation = new Reservation({
+            startDate: startDate,
+            endDate: endDate,
+            roomId: roomId,
+            userId: userId
         });
+
+        await reservation.save();
+        res.redirect('/dashboard'); // Rediriger vers le tableau de bord ou une autre page
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Une erreur est survenue lors de la réservation.");
+    }
 };
+
+// Fonction pour vérifier la disponibilité de la salle pour les dates spécifiées
+async function checkRoomAvailability(startDate, endDate, roomId) {
+    try {
+        // Vérifier s'il existe des réservations pour la salle et les dates spécifiées
+        const existingReservation = await Reservation.findOne({
+            roomId: roomId,
+            $or: [
+                { startDate: { $lt: endDate }, endDate: { $gt: startDate } },
+                { startDate: { $eq: startDate }, endDate: { $eq: endDate } }
+            ]
+        });
+
+        return !existingReservation; // Retourne true si la salle est disponible, sinon false
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
